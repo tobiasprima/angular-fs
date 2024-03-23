886c5fee562d7b36e6e96a2e26d7cb422b731942
Tobias Prima <primatjahja@gmail.com>
2024-03-23 23:25:34 +0800

    feat: styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TodoComponent } from './todo/todo.component';
     TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { remult } from 'remult';
+import { Task } from 'src/shared/task';
 
 @Component({
   selector: 'app-todo',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
+  tasks: Task[] = []
+  newTaskTitle= ''
 
+  taskRepo = remult.repo(Task)
+
+  ngOnInit(){
+    this.taskRepo.find({where: {
+      completed: undefined,
+    }}).then((tasks)=> (this.tasks = tasks))
+  }
 }

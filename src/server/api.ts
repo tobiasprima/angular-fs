403542a869e0a3b1bbc9993e4344d6bb53cc0abc
Tobias Prima <primatjahja@gmail.com>
2024-03-24 23:50:35 +0800

    feat: null protection

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,17 +1,15 @@
 
-import { configDotenv } from "dotenv";
 import { createPostgresDataProvider } from 'remult/postgres';
 import { remultExpress } from 'remult/remult-express';
 import { TasksController } from 'src/shared/TasksController';
 import { Task } from 'src/shared/task';
 
-configDotenv();
 
 export const api = remultExpress({
     entities: [Task],
     controllers: [TasksController],
     getUser: (req) => req.session!['user'],
     dataProvider: createPostgresDataProvider({
-        connectionString: process.env['DATABASE_URL']
+        connectionString: process.env['DATABASE_URL'] || 'postgres://postgres:MASTERKEY@localhost/postgres',
     }),
 })
